d55b10dea506badc5171ca46f3b892f087a62d05
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-15 16:39:35 -0600

    Added model classes for WelcomeMessage and Greeting

---
+++ src/main/java/com/abaiyat/vchatws/ui/model/WelcomeMessage.java
@@ -0,0 +1,17 @@
+package com.abaiyat.vchatws.ui.model;
+
+public class WelcomeMessage {
+    private String name;
+
+    public WelcomeMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
