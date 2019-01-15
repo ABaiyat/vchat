628f837a8be70ee54550488af6faa00cfda334b6
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-15 16:29:40 -0600

    Initial Commit

---
+++ src/main/java/com/abaiyat/vchatws/VchatWsApplication.java
@@ -0,0 +1,14 @@
+package com.abaiyat.vchatws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VchatWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VchatWsApplication.class, args);
+	}
+
+}
+
