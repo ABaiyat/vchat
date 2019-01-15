d3c1a5e35272b09d3358fb6548326f7e4cb8d1ad
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-15 17:34:50 -0600

    Added semantic-ui-react to React App

--- src/main/resources/client/src/App.js
+++ src/main/resources/client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+import {Button} from 'semantic-ui-react';
 
 class App extends Component {
   render() {
